163d28ef9315bbd05defed473103d1ea46d144ba
Costantino Martignetti <costa.martignetti@gmail.com>
2023-12-15 19:45:45 +0100

    asdfasf

--- prova.java
+++ prova.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        mi piaci perchè sei simpatico
+        System.err.println("ciaocome va");
     }
 }
