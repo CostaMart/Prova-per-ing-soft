0ed217e055093afaec0be9ac3276ffa84f9f8c74
Angelo Romano <102727644+Angelorom@users.noreply.github.com>
2023-12-21 20:11:04 +0100

    cambiamenti di prova.java

--- prova.java
+++ prova.java
@@ -3,5 +3,16 @@ public class HelloWorld {
         System.out.println("Hello, World!");
         System.err.println("ciaocome va");
         System.err.println("no non stai lavorando idiota");
+
+        System.out.println("Fibonacci di 5 è: " + fibonacci(5));
+    }
+    
+    public static int fibonacci(int x){
+        if(x==0) 
+            return x;
+        else if(x==1) 
+            return x;
+        else 
+            return fibonacci(x-1)+ fibonacci(x-2);
     }
 }
